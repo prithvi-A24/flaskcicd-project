5dd459c50c51ac0c62b39daede4770693ccb3e7e
prithvi-A24 <squareshift.co>
2026-07-07 10:18:22 +0000

    code changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD Project! and docker push "
+    return "Hello from Jenkins CI/CD Project! and docker push and deploy "
 
 @app.route("/health")
 def health():
